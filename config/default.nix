5658f8afce8afa1a0641480acf991e935bc24207
Adam Klepac <djklepy@gmail.com>
2025-07-17 10:42:28 +0200

    testing disabled colorschemes

--- config/default.nix
+++ config/default.nix
@@ -22,7 +22,7 @@
   ];
 
   bufferlines.enable = lib.mkDefault true;
-  colorschemes.enable = lib.mkDefault true;
+  colorschemes.enable = lib.mkDefault false;
   completion.enable = lib.mkDefault true;
   dap.enable = lib.mkDefault true;
   filetrees.enable = lib.mkDefault true;
