d322056204e5191f05b0f1352d159d54cbcc7766
Adam Klepac <djklepy@gmail.com>
2025-07-17 09:03:41 +0200

    fixed broken packages

--- config/keys.nix
+++ config/keys.nix
@@ -457,6 +457,38 @@
         key = "<Esc>";
         action = "<cmd>nohlsearch<CR>";
       }
+
+      # Set harpoon keymaps
+      { 
+	mode = "n";
+	key = "<leader>ha"; 
+	action.__raw = "function() require'harpoon':list():add() end"; 
+      }
+      { 
+	mode = "n"; 
+	key = "<C-e>"; 
+	action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end"; 
+      }
+      { 
+	mode = "n"; 
+	key = "<leader>hj"; 
+	action.__raw = "function() require'harpoon':list():select(1) end"; 
+      }
+      { 
+	mode = "n";
+	key = "<leader>hk";
+	action.__raw = "function() require'harpoon':list():select(2) end"; 
+      }
+      { 
+	mode = "n";
+	key = "<leader>hl"; 
+	action.__raw = "function() require'harpoon':list():select(3) end"; 
+      }
+      { 
+	mode = "n"; 
+	key = "<leader>hm"; 
+	action.__raw = "function() require'harpoon':list():select(4) end"; 
+      }
     ];
   };
 }
