b8f4117fed686cb6a4d21bb52450e78b02df04d1
Adam Klepac <djklepy@gmail.com>
2025-07-17 15:22:44 +0200

    added tex lua snippets

--- config/completion/default.nix
+++ config/completion/default.nix
@@ -1,15 +1,5 @@
-{
-  lib,
-  config,
-  ...
-}:
-{
-  imports = [
-    ./cmp.nix
-    ./codeium.nix
-    ./copilot.nix
-    ./lspkind.nix
-  ];
+{ lib, config, ... }: {
+  imports = [ ./cmp.nix ./codeium.nix ./copilot.nix ./lspkind.nix ];
 
   options = {
     completion.enable = lib.mkEnableOption "Enable completion module";
@@ -17,7 +7,7 @@
   config = lib.mkIf config.completion.enable {
     cmp.enable = lib.mkDefault true;
     codeium.enable = lib.mkDefault false;
-    copilot.enable = lib.mkDefault false;
+    copilot.enable = lib.mkDefault true;
     lspkind.enable = lib.mkDefault true;
   };
 }
