51ef636b75b4d8e0f540f09df50601b00948d03a
Adam Klepac <djklepy@gmail.com>
2025-07-17 15:59:37 +0200

    added vimtex support

--- config/languages/default.nix
+++ config/languages/default.nix
@@ -1,14 +1,6 @@
-{
-  lib,
-  config,
-  ...
-}:
-{
-  imports = [
-    ./treesitter-nvim.nix
-    ./nvim-jdtls.nix
-    ./nvim-lint.nix
-  ];
+{ lib, config, ... }: {
+  imports =
+    [ ./treesitter-nvim.nix ./nvim-jdtls.nix ./nvim-lint.nix ./vimtex.nix ];
 
   options = {
     languages.enable = lib.mkEnableOption "Enable languages module";
@@ -17,5 +9,6 @@
     treesitter-nvim.enable = lib.mkDefault true;
     jdtls.enable = lib.mkDefault true;
     nvim-lint.enable = lib.mkDefault true;
+    vimtex.enable = lib.mkDefault true;
   };
 }
