7bc132029665279fa2a8f17d3bcfaa5d76353803
Adam Klepac <djklepy@gmail.com>
2025-07-17 15:37:38 +0200

    more snippets fixes

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -19,9 +19,7 @@
 
             "<Tab>" = ''
               cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
+                if luasnip.expand_or_jumpable() then
                   luasnip.expand_or_jump()
                 else
                   fallback()
@@ -31,9 +29,7 @@
 
             "<S-Tab>" = ''
               cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                elseif luasnip.locally_jumpable(-1) then
+                if luasnip.locally_jumpable(-1) then
                   luasnip.jump(-1)
                 else
                   fallback()
