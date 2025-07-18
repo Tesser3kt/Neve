217bfb29308000eca2ea30639421e29e8d557c27
Adam Klepac <djklepy@gmail.com>
2025-07-18 13:58:19 +0200

    changed vimtex to use texlive-full

--- config/languages/vimtex.nix
+++ config/languages/vimtex.nix
@@ -1,8 +1,10 @@
-{ lib, config, ... }: {
+{ lib, config, pkgs, ... }: {
   options = { vimtex.enable = lib.mkEnableOption "Enable vimtex module"; };
-  config =
-    lib.mkIf config.vimtex.enable { 
-      plugins.vimtex = { enable = true; }; 
-      globals.vimtex_view_method = lib.mkForce "zathura";
+  config = lib.mkIf config.vimtex.enable {
+    plugins.vimtex = {
+      enable = true;
+      texlivePackage = pkgs.texlive.combined.scheme-full;
     };
+    globals.vimtex_view_method = lib.mkForce "zathura";
+  };
 }
